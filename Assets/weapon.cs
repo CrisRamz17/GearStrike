d159dc8c78b624a13cbb216ccf791093d4b43214
PaladinDigital117 <gswaner77@gmail.com>
2023-10-15 17:02:05 -0700

    Implemented basic shooting
    Weapon fires a bullet which destroys enemies but not walls. 3 Second timer to get rid of the bullet if it doesnt hit anything.

---
+++ Assets/weapon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform bulletSpawnpoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnpoint.position, bulletSpawnpoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnpoint.forward * bulletSpeed;
+        }
+    }
+}
